3084e6e7a5c063c87a85f209656750e5ce17b26f
OliverP255 <oliverpryce.business@gmail.com>
2025-07-11 18:50:02 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import threading
 from main import run_flask
 from websocket_server import run_websocket_server
+from flask_app import run_flask_app
 
 if __name__ == "__main__":
+    run_flask_app()
+    
     flask_thread = threading.Thread(target=run_flask)
     websocket_thread = threading.Thread(target=run_websocket_server)
 
@@ -11,3 +14,4 @@ if __name__ == "__main__":
 
     flask_thread.join()
     websocket_thread.join()
+
