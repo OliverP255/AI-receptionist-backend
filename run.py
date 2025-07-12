fc70cdf1f458fbb4e15457fae3b0acbf1fa61b52
OliverP255 <oliverpryce.business@gmail.com>
2025-07-12 19:43:21 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-import threading
 from websocket_server import run_websocket_server
 
 if __name__ == "__main__":
-    # Just run the WebSocket server (no Flask anymore)
-    websocket_thread = threading.Thread(target=run_websocket_server)
-
-    websocket_thread.start()
-    websocket_thread.join()
+    run_websocket_server()
